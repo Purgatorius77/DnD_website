163021727f2761395a58652fb9ac7c4dca8ab182
Purgatorius77 <erikvandervelden77@gmail.com>
2025-12-26 14:44:35 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//main.js
+
+import { initMonsterFilters } from "./filters/monsterfilters.js";
+import { initSpellFilters } from "./filters/spellfilters.js";
+import { initMonsterStatblock } from "./monsters/monsterstatblock.js";
+import { initSpellStatblock } from "./spells/spellstatblock.js";
+import { initViewSwitcher } from "./ui/viewswitcher.js";
+import { loadAllData } from "./data/dataloader.js";
+import { initCombatTracker } from "./combat/combattracker.js";
+import { initRulesFilters } from "./filters/rulesfilters.js";
+import { initRulesStatblock } from "./rules/rules.js"; // <--- import added
+import { initCharacterFilters } from "./filters/characterfilter.js";
+import { initCharacterStatblock } from "./characters/characters.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const { monsters, spells, monsterFluff } = await loadAllData();
+
+  // 🔑 single shared store
+  window.appState = {
+    monsters,
+    spells,
+    fluff: monsterFluff
+  };
+
+  initMonsterStatblock(monsters, monsterFluff, spells);
+  initSpellStatblock(spells);
+
+  initMonsterFilters(monsters);
+  initSpellFilters(spells);
+  initCombatTracker(monsters);
+  initRulesFilters();
+  initRulesStatblock();
+  initCharacterFilters();
+  initCharacterStatblock();
+  initViewSwitcher();
+
+});
