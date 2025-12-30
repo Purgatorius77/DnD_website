2e6d3e87993ffeeef9773a72754db1c54572ab4c
Purgatorius77 <erikvandervelden77@gmail.com>
2025-12-30 09:16:14 +0100

    Add files via upload

--- js/fluff/fluff.js
+++ js/fluff/fluff.js
@@ -1,6 +1,22 @@
 export function initFluffStatblock() {
     const fluffStatblock = document.getElementById("fluff-statblock");
 
+    document.addEventListener("showFluff", () => {
+showFluff();
+    });
 
+showFluff();
+
+function showFluff() {
+    fluffStatblock.innerHTML = `
+
+  <div class="home-text">
+    <h1>Welcome to my D&D page</h1>
+    <p>
+D&D lore is coming soon!
+     </p>
+  </div>
+`;
+}
 
 }
