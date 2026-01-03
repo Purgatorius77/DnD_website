1ca3f892ada727ddf2ea10608455a82b31455f98
Purgatorius77 <erikvandervelden77@gmail.com>
2026-01-03 11:39:32 +0100

    Add files via upload

--- js/filters/flufffilter.js
+++ js/filters/flufffilter.js
@@ -1,3 +1,19 @@
 export function initFluffFilters() {
   
+// rulesfilters.js
+
+
+  const loreBtn = document.getElementById("loreBtn");
+const worldBtn = document.getElementById("worldBtn");
+
+  loreBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showLore"));
+  });
+
+  worldBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showWorld"));
+  });
+
+
 }
+
