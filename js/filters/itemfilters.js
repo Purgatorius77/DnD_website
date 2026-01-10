ed5174e340e36921b636d643d396ece07d1fff2f
Purgatorius77 <erikvandervelden77@gmail.com>
2026-01-10 09:55:14 +0100

    Add files via upload

--- js/filters/itemfilters.js
+++ js/filters/itemfilters.js
@@ -3,7 +3,13 @@ export function initItemsFilters() {
   const basicItemsBtn = document.getElementById("basicItemsBtn");
   const itemsBtn = document.getElementById("itemsBtn");
   const magicalitemsBtn = document.getElementById("magicalitemsBtn");
-  const lootBtn = document.getElementById("lootBtn");
+  const treasureBtn = document.getElementById("treasureBtn");
+  const relicsBtn = document.getElementById("relicsBtn");
+  const gemsBtn = document.getElementById("gemsBtn");
+  const artBtn = document.getElementById("artBtn");
+  const arcanaBtn = document.getElementById("arcanaBtn");
+  const armamentsBtn = document.getElementById("armamentsBtn");
+  const implementsBtn = document.getElementById("implementsBtn");
   const filters = document.getElementById("items-filters");
 
   if (!basicItemsBtn || !itemsBtn || !filters) return;
@@ -23,7 +29,31 @@ export function initItemsFilters() {
     document.dispatchEvent(new CustomEvent("showMagicalItemsTable"));
   });
 
-      lootBtn.addEventListener("click", () => {
-    document.dispatchEvent(new CustomEvent("showLootTable"));
+    treasureBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showTreasureTable"));
+  });
+
+      gemsBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showGemsTable"));
+  });
+
+      arcanaBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showArcanaTable"));
+  });
+
+      armamentsBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showArmamentsTable"));
+  });
+
+      relicsBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showRelicsTable"));
+  });
+
+      implementsBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showImplementsTable"));
+  });
+
+    artBtn.addEventListener("click", () => {
+    document.dispatchEvent(new CustomEvent("showArtTable"));
   });
 }
